f1573754d845bcce1dfe01401afa458aa813e81b
Dragos Cristian Manta <dragos.manta@yahoo.ca>
2022-12-15 14:55:27 -0500

    Implemented transformer value policy

--- dag_gflownet/nets/transformer/gflownet.py
+++ dag_gflownet/nets/transformer/gflownet.py
@@ -1,108 +1,109 @@
 import jax.numpy as jnp
 import haiku as hk
 
-from dag_gflownet.nets.transformer.transformers import TransformerBlock
-from dag_gflownet.utils.gflownet import log_policy
+from dag_gflownet.nets.transformer.transformers import Transformer
 
 
-def gflownet(adjacency, mask):
-    """GFlowNet used in DAG-GFlowNet.
-
-    This GFlowNet uses a neural network architecture based on Linear
-    Transformers. It is composed of a common backbone of 3 Linear Transformers
-    layers, followed by two heads: one to compute the probability to stop the
-    sampling process, and another to compute the logits of transitioning to a
-    new graph, given that we didn't stop. Each head is composed of an additional
-    2 Linear Transformers layers, followed by a 3-layer MLP.
-
-    Note that each Linear Transformers layer takes an embedding obtained at the
-    previous layer of the network, as well as an embedding of the input adjacency
-    matrix (with a different embedding at each layer). This ensures that the
-    information about which edges are present in the graph is propagated as much
-    as possible.
-
-    The GFlowNet takes as an input a *single* adjacency matrix; this model is
-    later vmapped inside the `DAGGFlowNet` class.
-
+def value_policy_transformer(graphs, masks, x_dim, K):
+    """
     Parameters
     ----------
-    adjacency : jnp.DeviceArray
-        The adjacency matrix of a graph G. This array must have size `(N, N)`,
-        where `N` is the number of variables in G.
+    graphs: namedtuple `Graph` of (jraph._src.graph.GraphsTuple, jraph._src.graph.GraphsTuple)
+        Each element of the tuple is a batch of graphs encoded as a single GraphsTuple.
+        Each graph in the batch corresponds to the current
+        state in a parallel instantiation of the environment (there are
+        `batch_size` parallel environments).
+        The distinction between the two elements in the tuple is that the first
+        element encodes in the node features the node identities, while the
+        second element encodes the node values (which are discrete).
 
-    mask : jnp.DeviceArray
-        The mask for the valid actions that can be taken. This array must have
-        size `(N, N)`, where `N` is the number of variables in G.
 
+    masks: np.ndarray of shape (batch_size, h_dim+x_dim)
+        Batch of masks to prevent a given node from being sampled twice by the clique policy.
+        In addition, we also mask the nodes which are not part of a current incomplete
+        clique. masks[i, j] = 0 iff node j from batch i is unavailable
+        for sampling at this step. h_dim is the maximal number of nodes to sample from.
+        In our current setting, it corresponds to the number of nodes in the ground truth graph.
+    x_dim: int
+        Number of low-level variables.
+    K: int
+        Number of different discrete values that the nodes can take.
     Returns
     -------
-    logits : jnp.DeviceArray
-        The logits to compute P(G' | G) the probability of transitioning to a
-        new graph G' given G (including terminating, via the terminal state s_f).
-        This array has size `(N ** 2 + 1,)`, where `N` is the number of variables.
+    logits_values: jnp.DeviceArray of shape (batch_size, K)
+        Unnormalized log probabilities for the categorical distribution over the
+        K choices of the value for the target node to be sampled
+    log_flows: jnp.DeviceArray of shape (batch_size,)
+        Estimated log flow passing through the current state.
     """
-    # Create the edges as pairs of indices (source, target)
-    num_variables = adjacency.shape[0]
-    indices = jnp.arange(num_variables**2)
-    sources, targets = jnp.divmod(indices, num_variables)
-    edges = jnp.stack((sources, num_variables + targets), axis=1)
 
-    # Embedding of the edges
-    embeddings = hk.Embed(2 * num_variables, embed_dim=128)(edges)
-    embeddings = embeddings.reshape(num_variables**2, -1)
+    assert K == 2
 
-    # Reshape the adjacency matrix
-    adjacency = adjacency.reshape(num_variables**2, 1)
+    batch_size, num_variables = masks.shape
+    h_dim = num_variables - x_dim
+    masks = masks[:, :h_dim]
+    current_sampling_feature = num_variables + K + 1
 
-    # Apply common body
-    num_layers = 5
-    for i in range(3):
-        embeddings = TransformerBlock(
-            num_heads=4,
-            key_size=64,
-            embedding_size=128,
-            init_scale=2.0 / num_layers,
-            widening_factor=2,
-            name=f"body_{i+1}",
-        )(embeddings, adjacency)
+    transformer = Transformer(num_heads=4, num_layers=6, key_size=32, dropout_rate=0.0)
 
-    # Apply individual heads
-    logits = logits_head(embeddings, adjacency)
-    stop = stop_head(embeddings, adjacency)
+    # Embedding of the nodes & edges
+    node_embeddings_list = hk.Embed(num_variables + K + 2, embed_dim=128)
+    """
+    + 2 because we need to reserve a special embedding for: 
+    1) the (target) node with the missing value (to be sampled),     
+    2) the dummy index for nodes that are "padded" so that the GraphsTuple 
+        has a consistent size for JAX compilation
+    """
 
-    # Return the logits
-    return log_policy(logits, stop, mask)
+    # Preparing a separate copy of the embeddings for the flow estimator
+    # Setting the target node feature to be the same as the unobserved ones
+    flow_estimator_values_nodes = jnp.where(
+        graphs.values.nodes == current_sampling_feature,
+        num_variables + K,
+        graphs.values.nodes,
+    )
+    flow_estimator_value_embeddings = node_embeddings_list(flow_estimator_values_nodes)
 
+    # Embeddings for the policy head
+    structural_embeddings = node_embeddings_list(graphs.structure.nodes)
+    value_embeddings = node_embeddings_list(graphs.values.nodes)
+    node_embeddings = jnp.reshape(
+        structural_embeddings + value_embeddings, (batch_size, -1, 128)
+    )
 
-def logits_head(embeddings, adjacency):
-    num_layers = 5
+    # embeddings for the flow estimator
+    flow_estimator_node_embeddings = jnp.reshape(
+        structural_embeddings + flow_estimator_value_embeddings, (batch_size, -1, 128)
+    )
 
-    for i in range(2):
-        embeddings = TransformerBlock(
-            num_heads=4,
-            key_size=64,
-            embedding_size=128,
-            init_scale=2.0 / num_layers,
-            widening_factor=2,
-            name=f"head_logits_{i+1}",
-        )(embeddings, adjacency)
+    node_features = transformer(node_embeddings)
+    flow_estimator_node_features = transformer(flow_estimator_node_embeddings)
 
-    logits = hk.nets.MLP([256, 128, 1])(embeddings)
-    return jnp.squeeze(logits, axis=-1)
+    all_logits = hk.nets.MLP([128, K], name="logit")(node_features)
+    all_logits = jnp.reshape(
+        all_logits, (batch_size * num_variables, -1)
+    )  # Prepare for indexing with targets_ix
 
+    targets = (
+        graphs.values.nodes[: batch_size * num_variables] == current_sampling_feature
+    )
 
-def stop_head(embeddings, adjacency):
-    num_layers = 5
+    targets_ix = jnp.nonzero(targets, size=batch_size)
 
-    for i in range(2):
-        embeddings = TransformerBlock(
-            num_heads=4,
-            key_size=64,
-            embedding_size=128,
-            init_scale=2.0 / num_layers,
-            widening_factor=2,
-            name=f"head_stop_{i+1}",
-        )(embeddings, adjacency)
+    # OUTPUT: Computing policy logits
+    target_logits = all_logits[targets_ix]
 
-    mean = jnp.mean(embeddings, axis=-2)  # Average over edges
-    return hk.nets.MLP([256, 128, 1])(mean)
+    # Initialize the temperature parameter to 1
+    temperature = hk.get_parameter("temperature", (), init=hk.initializers.Constant(1))
+
+    # OUTPUT: Computing log_flows
+    flow_estimator_node_features = jnp.reshape(
+        flow_estimator_node_features, (batch_size, num_variables * 128)
+    )
+
+    log_flows = hk.nets.MLP([num_variables * 128, 1], name="log_flows")(
+        flow_estimator_node_features
+    )
+    log_flows = jnp.squeeze(log_flows, axis=1)
+
+    return (target_logits / temperature, log_flows)

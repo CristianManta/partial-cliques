fix masking bug

dag_gflownet/utils/data.py
@@ -193,7 +193,9 @@ def get_str_rep(nodes, model):
     return set([sorted(all_nodes)[n] for n in nodes])
 
 
-def get_clique_selection_mask(gfn_state: tuple, unobserved_cliques: list, K: int):
+def get_clique_selection_mask(
+    gfn_state: tuple, unobserved_cliques: list, K: int, h_dim: int
+):
     """
     Given a GFN state and a mutable representation of unfinished cliques,
     return a mask of eligible variables for the clique selection policy.
@@ -233,12 +235,16 @@ def get_clique_selection_mask(gfn_state: tuple, unobserved_cliques: list, K: int
 
     N = len(gfn_state[0])
     active_vars = set(np.nonzero(gfn_state[2] & gfn_state[0])[0].flatten())
+    observed_vars = set(np.nonzero(gfn_state[0])[0].flatten())
     eligible_cliques = list(
         filter(lambda c: c.issuperset(active_vars), unobserved_cliques)
     )
-    eligible_vars = set().union(*eligible_cliques) - set(active_vars)
+    eligible_vars = (
+        set().union(*eligible_cliques) - set(active_vars) - set(observed_vars)
+    )
 
     mask = np.zeros(N)
+    eligible_vars = list(filter(lambda x: x < h_dim, eligible_vars))
     if len(eligible_vars) == 0 or len(active_vars) == 0:
         mask = 1 - gfn_state[0]
     else:
Manually ported the KL computation fix from Edward env_batching branch

dag_gflownet/gflownet.py
@@ -236,6 +236,7 @@ class DAGGFlowNet:
         logs = {
             "is_exploration": None,
             "logpf": logpf,
+            "logz": log_flow,
         }
         return (actions, key, logs)
 
@@ -263,9 +264,12 @@ class DAGGFlowNet:
         log_p_hat = log_flow - log_true_partition_fn
         return log_p_hat, forward_key
 
-    def compute_reverse_kl(self, full_observations, full_cliques, traj_pf, ugm_model):
+    def compute_reverse_kl(
+        self, full_observations, full_cliques, traj_pf, log_marginal, ugm_model
+    ):
         # compute the reverse KL(GFN || GT)
         assert full_observations.shape[0] == traj_pf.shape[0]
+        assert full_observations.shape[0] == log_marginal.shape[0]
         factors = ugm_model.get_factors()
         # for every sample, compute the likelihood under the ugm_model
         kl_terms = []
@@ -283,11 +287,8 @@ class DAGGFlowNet:
                     ]
                 )
             # store the KL term
-            kl_terms.append(
-                traj_pf[i]
-                - total_log_potential
-                + np.log(ugm_model.get_partition_function())
-            )
+            kl_terms.append(traj_pf[i] + log_marginal - total_log_potential)
+            # + np.log(ugm_model.get_partition_function())
         return jnp.mean(jnp.array(kl_terms))
 
     @partial(jit, static_argnums=(0, 4, 5))

train.py
@@ -244,13 +244,16 @@ def main(args):
                     MLL=f"{log_p_hat_x_eval[0]:.2f}",
                 )
 
-                if (train_steps + 1) % args.evaluate_every == 0:
+                if (train_steps) % args.evaluate_every == 0:
                     # evaluete the GFN by sampling complete trajectories
                     eval_full_trajectories = []
                     eval_logpf = []
+                    eval_logz = []
+                    eval_log_marginal = []
                     eval_obs = eval_env.reset()
                     for _ in range(100):
                         logpf = 0.0
+                        logz = 0.0
                         eval_obs["graphs_tuple"] = to_graphs_tuple(
                             full_cliques, eval_obs["gfn_state"], args.K, args.x_dim
                         )
@@ -265,10 +268,24 @@ def main(args):
                         )
                         eval_obs, energies, dones = eval_env.step(actions)
                         logpf += logs["logpf"]
+                        logz += logs["logz"]
 
                         if dones[0][0]:
                             eval_full_trajectories.append(eval_obs["gfn_state"][0][1])
                             eval_logpf.append(logpf)
+                            eval_logz.append(logz)
+                            eval_log_marginal.append(
+                                np.log(
+                                    x_factors_values[
+                                        tuple(
+                                            [
+                                                eval_obs["gfn_state"][0][1][i]
+                                                for i in range(args.x_dim)
+                                            ]
+                                        )
+                                    ]
+                                )
+                            )
                             eval_obs = eval_env.reset()
                             logpf = 0.0
                     # calculate and print reverse KL
@@ -276,6 +293,7 @@ def main(args):
                         full_observations=jnp.stack(eval_full_trajectories, axis=0),
                         full_cliques=full_cliques,
                         traj_pf=jnp.array(eval_logpf),
+                        log_marginal=jnp.array(eval_log_marginal),
                         ugm_model=true_ugm,
                     )
                     print(f"Reverse KL: {reverse_kl}")